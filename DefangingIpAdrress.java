97ffeaf0d4c95170c523d46671d4cb9070ae9aca
sachi41999 <sachisabya41999@gmail.com>
2024-02-01 03:07:42 +0530

    1st_question

---
+++ DefangingIpAdrress.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class DefangingIpAdrress{
+    
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        String adress=sc.nextLine();
+
+        DefangingIpAdrress dip=new DefangingIpAdrress();
+        System.out.println(dip.modifyDefangingIPAddress(adress));
+
+        sc.close();
+    }
+
+    public String modifyDefangingIPAddress(String address){
+        String new_address="";
+        for(int i=0;i<address.length();i++){
+            if(String.valueOf(address.charAt(i)).equals(".")){
+                new_address+="[.]";
+                continue;
+            }
+            new_address+=address.charAt(i);
+        }
+
+        return new_address;
+    }
+}
